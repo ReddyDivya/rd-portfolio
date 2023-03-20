f3747498176da64bf36442f748eaa0e53dd0e728
Divya <divyareddy0205@gmail.com>
2023-03-19 23:25:11 -0700

    rd-portfolio

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from "./App";
-import "./index.css";
+
 
 //rendering app component into root element
 const root = ReactDOM.createRoot(document.getElementById("root"));
